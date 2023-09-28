1655108ce0fbb7292cf5a1b96e9a521a0ea7931c
Prakash7078 <20bq1a05j5@vvit.net>
2023-09-28 12:58:04 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import "./header.css";
 import { nav } from "../data/Data";
 import { Link } from "react-router-dom";
 import logo from "./wool-logo.png";
+import { Avatar } from "@material-tailwind/react";
+import avatar from './avatar.jfif'
 const Navbar = () => {
   const [navList, setNavList] = useState(false);
 
@@ -24,13 +26,11 @@ const Navbar = () => {
               ))}
             </ul>
           </div>
-          <div className="button flex">
+          <div className="button flex items-center">
             <h4>
               <span>2</span> My List
             </h4>
-            <button className="btn1">
-              <i className="fa fa-sign-out"></i> Sign In
-            </button>
+            <Avatar src={avatar} alt="avatar" />
           </div>
 
           <div className="toggle">
